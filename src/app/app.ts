fae42c00c1d7a86e8d63cc7dc036b7f612c7781e
Jose <Jmhr1997@gmail.com>
2025-07-22 21:49:09 -0300

    ..

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './navbar/navbar';
+import { PruebamoduloModule } from './pruebamodulo/pruebamodulo-module';
+import { Student } from './student/student';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PruebamoduloModule,Student,Navbar,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
